ca99ef3d9ded84b39627a5b8c583281d8a2894c6
MSharkI <42243051+MSharkI@users.noreply.github.com>
2024-02-04 11:09:23 +0300

    Initial commit

---
+++ logs/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace logs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.intTextBox = new System.Windows.Forms.TextBox();
+            this.dateTextBox = new System.Windows.Forms.TextBox();
+            this.charTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // intTextBox
+            // 
+            this.intTextBox.Location = new System.Drawing.Point(141, 155);
+            this.intTextBox.Name = "intTextBox";
+            this.intTextBox.Size = new System.Drawing.Size(100, 22);
+            this.intTextBox.TabIndex = 0;
+            // 
+            // dateTextBox
+            // 
+            this.dateTextBox.Location = new System.Drawing.Point(340, 155);
+            this.dateTextBox.Name = "dateTextBox";
+            this.dateTextBox.Size = new System.Drawing.Size(100, 22);
+            this.dateTextBox.TabIndex = 1;
+            // 
+            // charTextBox
+            // 
+            this.charTextBox.Location = new System.Drawing.Point(536, 155);
+            this.charTextBox.Name = "charTextBox";
+            this.charTextBox.Size = new System.Drawing.Size(100, 22);
+            this.charTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Integer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(563, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Char";
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(351, 242);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 6;
+            this.submitButton.Text = "Convert";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.charTextBox);
+            this.Controls.Add(this.dateTextBox);
+            this.Controls.Add(this.intTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox intTextBox;
+        private System.Windows.Forms.TextBox dateTextBox;
+        private System.Windows.Forms.TextBox charTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
+
---
+++ logs/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NLog;
+
+namespace logs
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int intValue = Convert.ToInt32(intTextBox.Text);
+                DateTime dateValue = Convert.ToDateTime(dateTextBox.Text);
+                char charValue = Convert.ToChar(charTextBox.Text);
+
+                MessageBox.Show($"Целое число: {intValue}\nДата: {dateValue}\nСимвол: {charValue}", "Успех");
+            }
+            catch (FormatException ex)
+            {
+                logger.Error(ex, "Ошибка формата. Пожалуйста, введите корректные значения.");
+                throw;
+            }
+            catch (OverflowException ex)
+            {
+                logger.Error(ex, "Ошибка переполнения. Пожалуйста, введите более подходящие значения.");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, $"Произошла ошибка: {0}", ex.Message);
+            }
+        }
+    }
+}
